c01e0d9fceaedb08a62d614b9ad7d315dba119c7
Karam K. Afandi <77574018+karamlyy@users.noreply.github.com>
2023-06-10 10:55:58 +0400

    empty

--- app/src/main/java/com/emilabdurahmanli/zireddinapplication/core/bottom_navigation_fragments/HoroscopeFragment.kt
+++ app/src/main/java/com/emilabdurahmanli/zireddinapplication/core/bottom_navigation_fragments/HoroscopeFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
 import com.emilabdurahmanli.zireddinapplication.R
+import com.emilabdurahmanli.zireddinapplication.databinding.FragmentHoroscopeBinding
+import com.emilabdurahmanli.zireddinapplication.databinding.ToolBarBinding
 
 
 class HoroscopeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHoroscopeBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,8 @@ class HoroscopeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.backTv.visibilty = GONE
+        binding.toolbar.
     }
 
     override fun onCreateView(
@@ -25,7 +32,10 @@ class HoroscopeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_horoscope, container, false)
+        binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+
+
+        return inflater.inflate(binding.root, container, false)
     }
 
 
